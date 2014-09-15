db04d96b194a748432ba06c72d4323fe681e657a
Wesley <wesleykoorneef@hotmail.com>
2014-09-15 10:27:18 +0200

    Ik weet vrijwel zeker dat het werkt, alleen zonder dat de site op internet staat zal deze php code niks kunnen. Ik heb 2x een contact.php geschreven, omdat ik dacht dat de eerste fout was en die bleek toch goed te zijn.

---
+++ contact html/php/public_html/contact.php
@@ -0,0 +1,71 @@
+<?php
+if(isset($_POST["email"])) {
+    
+    // Hier komt informatie over waar de email naartoe gaat //
+    $email_to="scrumlife@outlook.com"; // of ander email adres ik had er 1 aangemaakt voor de gein //
+    $email_subject="Filmpje contactformulier probleem"; // onderwerp van de email die verstuurd wordt //
+    $email_from="Filmpje bioscopen"; // kunnen we nog een andere naam voor verzinnen //
+    
+    // foutmeldingen
+    function probleem($probleem2) {
+        echo "Sorry voor het ongemak, maar het blijkt dat er problemen zijn onstaan in het formulier dat u ons heeft gestuurd.";
+        echo "Hieronder wordt aangegeven wat de problemen zijn.";
+        echo $probleem2. "<br> <br>";
+        echo "Alstublieft verander deze fouten in uw formulier en probeer opnieuw.<br/>";
+        die();
+    }
+    
+    $voornaam = $_POST('voornaam');
+    $achternaam = $_POST('achternaam');
+    $email = $_POST['email'];
+    $probleem = $_POST['probleem'];
+    
+    // controleren
+    if(!isset($voornaam) ||
+        !isset($achternaam) ||
+        !isset($email) ||
+        !isset($probleem)) {
+            died("Sorry voor het ongemak, maar het blijkt dat 1 of meerdere velden niet zijn ingevuld.");
+        }
+        
+        $foutmelding = "";
+        $emailverwachting = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2, 4}$/';  
+        /* alles tussen [] kan gebruikt worden in emails, dus []@[].[] en de {} geven aan 
+        hoeveel letters na de . mogen */
+        $voornaamverwachting = "/^[A-Za-z.'-]+$/";
+        $achternaamverwachting = "/^[A-Za-z.'-]+$/";
+        // alles tussen de [] kan gebruikt worden in de voor en achternaam //
+        
+        if(!preg_match($emailverwachting, $email)) {
+            $foutmelding .= "Het e-mailadres dat u heeft ingevoerd blijkt tekens te bevatten die niet geldig zijn.<br>";
+        }
+        
+        if(!preg_match($voornaamverwachting, $voornaam)) {
+            $foutmelding .= "De voornaam die u heeft ingevoerd blijkt tekens te bevatten die niet geldig zijn.<br>";
+        }
+        
+        if(!preg_match($achternaamverwachting, $achternaam)) {
+            $foutmelding .= "De achternaam die u heeft ingevoerd blijkt tekens te bevatten die niet geldig zijn.<br>";
+        }
+        
+        if(strlen($probleem) < 2) {
+            $foutmelding .= "Het aantal tekens dat u heeft geschreven is te weinig.<br>";
+        }
+        
+        if(strlen($foutmelding) > 0) {
+            died($foutmelding);
+        }
+        $emailbericht = "Formulier details kunt u beneden lezen. \n\n";
+        
+        $emailbericht .= "Voornaam:" . $voornaam . "\n";
+        $emailbericht .= "Achternaam:" . $achternaam . "\n";
+        $emailbericht .= "Email:" . $email . "\n";
+        $emailbericht .= "Probleem:" . $probleem . "\n";
+        // email headers
+        $headers = 'From: ' .$email_from . "\r\n". 'Beantwoorden:' . $email. "\r\n" .
+        'X-mailer: php/' . phpversion();
+        
+        @mail($email_to, $email_subject, $emailbericht, $headers);
+}
+
+?>
